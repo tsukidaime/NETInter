35481e8b1861b1752d45f20fa80f0fff7e8559fa
Aibek_Shulembekov <Aibek_Shulembekov@EPKZASTW0418>
2021-07-12 14:58:51 +0600

    Add project files.

---
+++ IQueryable Task/Expressions and IQueryable.Tasks.Week2_updated/Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Expressions.Task3.E3SQueryProvider
+{
+    public class ExpressionToFtsRequestTranslator : ExpressionVisitor
+    {
+        readonly StringBuilder _resultStringBuilder;
+
+        public ExpressionToFtsRequestTranslator()
+        {
+            _resultStringBuilder = new StringBuilder();
+        }
+
+        public string Translate(Expression exp)
+        {
+            Visit(exp);
+
+            return _resultStringBuilder.ToString();
+        }
+
+        #region protected methods
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable)
+                && node.Method.Name == "Where")
+            {
+                var predicate = node.Arguments[1];
+                Visit(predicate);
+
+                return node;
+            }
+            else if(node.Method.Name == "Equals")
+            {
+                VisitMember((MemberExpression)node.Object);
+                _resultStringBuilder.Append("(");
+                Visit(node.Arguments.First());
+                _resultStringBuilder.Append(")");
+                return node;
+            }
+            else if (node.Method.Name == "Contains")
+            {
+                VisitMember((MemberExpression)node.Object);
+                _resultStringBuilder.Append("(*");
+                Visit(node.Arguments.First());
+                _resultStringBuilder.Append("*)");
+                return node;
+            }
+            else if (node.Method.Name == "StartsWith")
+            {
+                VisitMember((MemberExpression)node.Object);
+                _resultStringBuilder.Append("(");
+                Visit(node.Arguments.First());
+                _resultStringBuilder.Append("*)");
+                return node;
+            }
+            else if (node.Method.Name == "EndsWith")
+            {
+                VisitMember((MemberExpression)node.Object);
+                _resultStringBuilder.Append("(*");
+                Visit(node.Arguments.First());
+                _resultStringBuilder.Append(")");
+                return node;
+            }
+            return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal:
+                    if (node.Left.NodeType == ExpressionType.MemberAccess) {
+                        Visit(node.Left);
+                        _resultStringBuilder.Append("(");
+                        Visit(node.Right);
+                        _resultStringBuilder.Append(")");
+                    }
+                    else
+                    {
+                        Visit(node.Right);
+                        _resultStringBuilder.Append("(");
+                        Visit(node.Left);
+                        _resultStringBuilder.Append(")");
+                    }
+                    break;
+                case ExpressionType.AndAlso:
+                    Visit(node.Left);
+                    _resultStringBuilder.Append("/");
+                    Visit(node.Right);
+                    break;
+                default:
+                    throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
+            };
+
+            return node;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            _resultStringBuilder.Append(node.Member.Name).Append(":");
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _resultStringBuilder.Append(node.Value);
+
+            return node;
+        }
+
+        #endregion
+    }
+}
